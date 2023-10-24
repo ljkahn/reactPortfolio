b23dfda1d38f846d2f1a3b1999a3cc6ea624b61a
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-10-24 17:51:39 -0600

    updated index.html, added components and views

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BucketList from './components/BucketList';
+
+function App() {
+  return (
+    <div className="bucket-app">
+      <BucketList />
+    </div>
+  );
+}
+
+export default App;
