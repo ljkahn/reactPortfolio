cf7bf391d0944408119ba91fcf001d1efea79bd2
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-10-29 17:37:07 -0600

    added toast functionality for successful submission

--- react-portfolio/src/pages/Contact.jsx
+++ react-portfolio/src/pages/Contact.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import emailjs from '@emailjs/browser';
 import { useRef, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 
 function Contact () {
   const form = useRef();
-  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  // Function that displays a success toast on bottom right of the page when form submission is successful
+  const toastifySuccess = () => {
+    toast('Form sent!', {
+      position: 'bottom-right',});
+    }
 
   const sendEmail = (e) => {
     e.preventDefault();
