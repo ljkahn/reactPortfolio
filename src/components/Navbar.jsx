d465779f74a014153cc01b6fc0a069a414f1abf0
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-11-20 15:23:49 -0700

    udpating image files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,37 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import brand from '../assets/workImages/brand.png';
-import PersonalAi from '../assets/siteImages/PersonalAi.png';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import brand from "../assets/workImages/brand.png";
+import Personal from "../assets/siteImages/PersonalAi.png";
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-transparent navbar ">
-    <Container className='d-flexjustify-content-between'>
-      <Navbar.Brand>
-        <img src={PersonalAi} alt='Lia Kahn' className='personal'/>
-        <img src={brand} alt='Lia Kahn Logo' className='nav-brand'/>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className='fs-1'>
-     
-          <Link to="/about" className='nav-link'>About</Link>
-          <Link to="/work" className='nav-link'>Portfolio</Link>
-          <Link to="/contact" className='nav-link'>Contact</Link>
-          <Link to="/resume" className='nav-link'>Resume</Link>
-        </Nav>
+      <Container className="d-flexjustify-content-between">
+        <Navbar.Brand>
+          <img src={Personal} alt="Lia Kahn" className="personal" />
+          <img src={brand} alt="Lia Kahn Logo" className="nav-brand" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="fs-1">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+            <Link to="/work" className="nav-link">
+              Portfolio
+            </Link>
+            <Link to="/contact" className="nav-link">
+              Contact
+            </Link>
+            <Link to="/resume" className="nav-link">
+              Resume
+            </Link>
+          </Nav>
         </Navbar.Collapse>
-    </Container>
-  </Navbar>
+      </Container>
+    </Navbar>
   );
 }
-
 
 export default Header;
